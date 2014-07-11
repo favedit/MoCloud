dae50915b557ef184be769451b42e6c9cf37377e
maochunyang <maochunyang@cyou-inc.com>
2014-07-11 15:46:09 +0800

    版本建立。

---
+++ batch/tomcat-cloud.manage-windows-8.0.9-x64.bat
@@ -0,0 +1,13 @@
+@CLS
+
+@SET JAVA_HOME=C:\Program Files\Java\jdk1.8.0_05
+@SET APP_ROOT=E:\ZW-MoCloud
+
+@SET CATALINA_OPTS=-Xms256m -Xmx512m
+@SET CATALINA_HOME=%APP_ROOT%\mt-server\tomcat-cloud.service-windows-8.0.9-x64
+@SET MOBJ_MODE=Logic
+@SET MOBJ_JS=Test
+@SET MOBJ_CONFIG=application-local-console
+@SET JAVA_OPTS=-Duser.mobj.mode=%MOBJ_MODE% -Duser.mobj.js=%MOBJ_JS% -Duser.mobj.config=%MOBJ_CONFIG%
+
+@"%CATALINA_HOME%\bin\catalina.bat" run
