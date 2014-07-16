53b1d0fd0493e6c4e252dad496d21417edf2f837
maochunyang <maochunyang@cyou-inc.com>
2014-07-16 10:03:46 +0800

    库修正。

--- copy_library.bat
+++ copy_library.bat
@@ -8,3 +8,9 @@ COPY /Y %HOME_SOURCE%\mo-common.jar %HOME_TARGET%
 COPY /Y %HOME_SOURCE%\mo-core.jar %HOME_TARGET%
 COPY /Y %HOME_SOURCE%\mo-logic.jar %HOME_TARGET%
 COPY /Y %HOME_SOURCE%\mo-web.jar %HOME_TARGET%
+
+@SET HOME_TARGET=%HOME%\mp-cloud-manage\webroot\WEB-INF\lib\
+COPY /Y %HOME_SOURCE%\mo-common.jar %HOME_TARGET%
+COPY /Y %HOME_SOURCE%\mo-core.jar %HOME_TARGET%
+COPY /Y %HOME_SOURCE%\mo-logic.jar %HOME_TARGET%
+COPY /Y %HOME_SOURCE%\mo-web.jar %HOME_TARGET%
