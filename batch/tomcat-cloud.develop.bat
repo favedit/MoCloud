06eeb4c78256b58ddad3a5199f60f838a912d24d
maochunyang <maochunyang@cyou-inc.com>
2015-01-22 10:38:08 +0800

    配置修正。

--- batch/tomcat-cloud.develop.bat
+++ batch/tomcat-cloud.develop.bat
@@ -1,10 +1,9 @@
 @CLS
 
-@SET JAVA_HOME=C:\Program Files\Java\jdk1.8.0_05
-@SET APP_ROOT=E:\ZW-MoCloud
+@SET APP_ROOT=%cd%\..
 
 @SET CATALINA_OPTS=-Xms256m -Xmx512m
-@SET CATALINA_HOME=%APP_ROOT%\mt-server\tomcat-cloud.manage-windows-8.0.9-x64
+@SET CATALINA_HOME=%APP_ROOT%\mt-server\tomcat-cloud.develop
 @SET MOBJ_MODE=Logic
 @SET MOBJ_JS=Test
 @SET MOBJ_CONFIG=application-local
