095dcb1f5f268d5cb8296686dd767cba8ebe36b2
maochunyang <maochunyang@cyou-inc.com>
2015-01-22 13:12:57 +0800

    目录调整。

--- copy_library.bat
+++ copy_library.bat
@@ -4,19 +4,28 @@
 @SET HOME_SOURCE=%HOME%\library
 
 @SET HOME_TARGET=%HOME%\mp-cloud-develop\webroot\WEB-INF\lib\
-COPY /Y %HOME_SOURCE%\mo-common.jar %HOME_TARGET%
-COPY /Y %HOME_SOURCE%\mo-core.jar %HOME_TARGET%
-COPY /Y %HOME_SOURCE%\mo-logic.jar %HOME_TARGET%
-COPY /Y %HOME_SOURCE%\mo-web.jar %HOME_TARGET%
+COPY /Y %HOME_SOURCE%\mo-common.jar  %HOME_TARGET%
+COPY /Y %HOME_SOURCE%\mo-core.jar    %HOME_TARGET%
+COPY /Y %HOME_SOURCE%\mo-logic.jar   %HOME_TARGET%
+COPY /Y %HOME_SOURCE%\mo-web.jar     %HOME_TARGET%
+COPY /Y %HOME_SOURCE%\mo-cloud.jar   %HOME_TARGET%
+COPY /Y %HOME_SOURCE%\mo-content.jar %HOME_TARGET%
+COPY /Y %HOME_SOURCE%\mo-utility.jar %HOME_TARGET%
 
 @SET HOME_TARGET=%HOME%\mp-cloud-design\webroot\WEB-INF\lib\
-COPY /Y %HOME_SOURCE%\mo-common.jar %HOME_TARGET%
-COPY /Y %HOME_SOURCE%\mo-core.jar %HOME_TARGET%
-COPY /Y %HOME_SOURCE%\mo-logic.jar %HOME_TARGET%
-COPY /Y %HOME_SOURCE%\mo-web.jar %HOME_TARGET%
+COPY /Y %HOME_SOURCE%\mo-common.jar  %HOME_TARGET%
+COPY /Y %HOME_SOURCE%\mo-core.jar    %HOME_TARGET%
+COPY /Y %HOME_SOURCE%\mo-logic.jar   %HOME_TARGET%
+COPY /Y %HOME_SOURCE%\mo-web.jar     %HOME_TARGET%
+COPY /Y %HOME_SOURCE%\mo-cloud.jar   %HOME_TARGET%
+COPY /Y %HOME_SOURCE%\mo-content.jar %HOME_TARGET%
+COPY /Y %HOME_SOURCE%\mo-utility.jar %HOME_TARGET%
 
 @SET HOME_TARGET=%HOME%\mp-cloud-content\webroot\WEB-INF\lib\
-COPY /Y %HOME_SOURCE%\mo-common.jar %HOME_TARGET%
-COPY /Y %HOME_SOURCE%\mo-core.jar %HOME_TARGET%
-COPY /Y %HOME_SOURCE%\mo-logic.jar %HOME_TARGET%
-COPY /Y %HOME_SOURCE%\mo-web.jar %HOME_TARGET%
+COPY /Y %HOME_SOURCE%\mo-common.jar  %HOME_TARGET%
+COPY /Y %HOME_SOURCE%\mo-core.jar    %HOME_TARGET%
+COPY /Y %HOME_SOURCE%\mo-logic.jar   %HOME_TARGET%
+COPY /Y %HOME_SOURCE%\mo-web.jar     %HOME_TARGET%
+COPY /Y %HOME_SOURCE%\mo-cloud.jar   %HOME_TARGET%
+COPY /Y %HOME_SOURCE%\mo-content.jar %HOME_TARGET%
+COPY /Y %HOME_SOURCE%\mo-utility.jar %HOME_TARGET%
