f44952ae2459b4cce3421360c4314b01dc73d0e4
favedit <favedit@hotmail.com>
2015-02-01 00:27:12 +0800

    代码修正。

--- mo-6-content/src/content-design/org/mo/cloud/content/design/frame/common/XColor3.java
+++ mo-6-content/src/content-design/org/mo/cloud/content/design/frame/common/XColor3.java
@@ -1,11 +1,11 @@
 package org.mo.cloud.content.design.frame.common;
 
-import org.mo.cloud.content.design.frame.base.XBaseColor;
+import org.mo.cloud.content.design.frame.base.XBaseColor3;
 
 //============================================================
-// <T>颜色编辑框。</T>
+// <T>颜色3编辑框。</T>
 //============================================================
-public class XColor
-      extends XBaseColor
+public class XColor3
+      extends XBaseColor3
 {
 }
--- mo-6-content/src/content-design/org/mo/cloud/content/design/frame/common/XColor4.java
+++ mo-6-content/src/content-design/org/mo/cloud/content/design/frame/common/XColor4.java
@@ -3,7 +3,7 @@ package org.mo.cloud.content.design.frame.common;
 import org.mo.cloud.content.design.frame.base.XBaseColor4;
 
 //============================================================
-// <T>数字编辑框。</T>
+// <T>颜色4编辑框。</T>
 //============================================================
 public class XColor4
       extends XBaseColor4
