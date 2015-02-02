8b6d0fb26e60b00b868cfcd773047f4ba471f8d3
maochunyang <maochunyang@cyou-inc.com>
2015-02-02 10:31:35 +0800

    控件修正。

---
+++ mo-6-content/src/content-design/org/mo/cloud/content/design/frame/common/XPageControl.java
@@ -0,0 +1,11 @@
+package org.mo.cloud.content.design.frame.common;
+
+import org.mo.cloud.content.design.frame.base.XBasePageControl;
+
+//============================================================
+// <T>页面控件。</T>
+//============================================================
+public class XPageControl
+      extends XBasePageControl
+{
+}
---
+++ mo-6-content/src/content-design/org/mo/cloud/content/design/frame/common/XPageSheet.java
@@ -0,0 +1,11 @@
+package org.mo.cloud.content.design.frame.common;
+
+import org.mo.cloud.content.design.frame.base.XBasePageSheet;
+
+//============================================================
+// <T>页面表单。</T>
+//============================================================
+public class XPageSheet
+      extends XBasePageSheet
+{
+}
