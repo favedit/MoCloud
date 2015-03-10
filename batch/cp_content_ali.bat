0e3936f90794ca0be0592d2d5844ebd65ef4b7b2
maochunyang <maochunyang@cyou-inc.com>
2015-03-10 16:20:54 +0800

    代码修正。

--- batch/cp_content_ali.bat
+++ batch/cp_content_ali.bat
@@ -6,3 +6,5 @@
 
 xcopy /E /I /Y %SOURCE%\mp-cloud-content\webroot %TARGET%\cloud.content\webroot
 xcopy /E /I /Y %HOME%\MoScript\source\ajs %TARGET%\cloud.content\script\ajs
+xcopy /E /I /Y %HOME%\MoScript\source\ejs %TARGET%\cloud.content\script\ejs
+xcopy /E /I /Y %HOME%\MoScript\source\design %TARGET%\cloud.content\script\design
