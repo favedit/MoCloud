a643462eecc8cfd973184d2487c917b8f6033d01
maochunyang <maochunyang@cyou-inc.com>
2015-03-27 17:55:57 +0800

    代码修正。

---
+++ mp-cloud-content/src/content-face/org/mo/content/face/solution/project/FProjectService.java
@@ -0,0 +1,74 @@
+package org.mo.content.face.solution.project;
+
+import org.mo.com.lang.EResult;
+import org.mo.com.lang.FFatalError;
+import org.mo.com.lang.FObject;
+import org.mo.com.xml.FXmlNode;
+import org.mo.content.core.solution.project.IC3dProjectConsole;
+import org.mo.core.aop.face.ALink;
+import org.mo.data.logic.ILogicContext;
+import org.mo.web.protocol.context.IWebContext;
+import org.mo.web.protocol.context.IWebInput;
+import org.mo.web.protocol.context.IWebOutput;
+
+//============================================================
+// <T>资源3D服务。</T>
+//============================================================
+public class FProjectService
+      extends FObject
+      implements
+         IProjectService
+{
+   // 项目控制台接口
+   @ALink
+   protected IC3dProjectConsole _projectConsole;
+
+   //============================================================
+   // <T>构造资源3D服务。</T>
+   //============================================================
+   public FProjectService(){
+   }
+
+   //============================================================
+   // <T>获取数据处理。</T>
+   //
+   // @param context 网络环境
+   // @param logicContext 逻辑环境
+   // @param input 网络输入
+   // @param output 网络输出
+   //============================================================
+   @Override
+   public EResult fetch(IWebContext context,
+                        ILogicContext logicContext,
+                        IWebInput input,
+                        IWebOutput output){
+      String typeCd = context.parameter("type_cd");
+      String serach = context.parameter("serach");
+      int pageSize = context.parameterAsInteger("page_size", 20);
+      int page = context.parameterAsInteger("page", 0);
+      // 设置输出节点
+      FXmlNode xoutput = output.config().createNode("ItemCollection");
+      xoutput.set("page_count", 0);
+      xoutput.set("page_size", pageSize);
+      xoutput.set("page", page);
+      //............................................................
+      // 查询数据
+      switch(typeCd){
+         case "private":
+            // 查找位图
+            _projectConsole.fetch(logicContext, xoutput, serach, pageSize, page);
+            break;
+         case "team":
+            // 查找声音
+            _projectConsole.fetch(logicContext, xoutput, serach, pageSize, page);
+            break;
+         case "share":
+            // 查找视频
+            _projectConsole.fetch(logicContext, xoutput, serach, pageSize, page);
+            break;
+         default:
+            throw new FFatalError("Unknown resource type. (type_cd={1})", typeCd);
+      }
+      return EResult.Success;
+   }
+}
---
+++ mp-cloud-content/src/content-face/org/mo/content/face/solution/project/IProjectService.java
@@ -0,0 +1,26 @@
+package org.mo.content.face.solution.project;
+
+import org.mo.com.lang.EResult;
+import org.mo.data.logic.ILogicContext;
+import org.mo.web.protocol.context.IWebContext;
+import org.mo.web.protocol.context.IWebInput;
+import org.mo.web.protocol.context.IWebOutput;
+
+//============================================================
+// <T>资源3D服务接口。</T>
+//============================================================
+public interface IProjectService
+{
+   //============================================================
+   // <T>获取数据处理。</T>
+   //
+   // @param context 网络环境
+   // @param logicContext 逻辑环境
+   // @param input 网络输入
+   // @param output 网络输出
+   //============================================================
+   EResult fetch(IWebContext context,
+                 ILogicContext logicContext,
+                 IWebInput input,
+                 IWebOutput output);
+}
