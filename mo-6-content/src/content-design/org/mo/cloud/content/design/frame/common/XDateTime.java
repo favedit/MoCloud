c5f5d767ec9aa758596e232c0858573051afa495
favedit <favedit@hotmail.com>
2015-04-05 22:53:17 +0800

    代码修正。

---
+++ mo-6-content/src/content-design/org/mo/cloud/content/design/frame/common/XDateTime.java
@@ -0,0 +1,11 @@
+package org.mo.cloud.content.design.frame.common;
+
+import org.mo.cloud.content.design.frame.base.XBaseDateTime;
+
+//============================================================
+// <T>时间编辑框。</T>
+//============================================================
+public class XDateTime
+      extends XBaseDateTime
+{
+}
---
+++ mo-6-content/src/content-design/org/mo/cloud/content/design/frame/common/XProgressBar.java
@@ -0,0 +1,11 @@
+package org.mo.cloud.content.design.frame.common;
+
+import org.mo.cloud.content.design.frame.base.XBaseProgressBar;
+
+//============================================================
+// <T>进度条。</T>
+//============================================================
+public class XProgressBar
+      extends XBaseProgressBar
+{
+}
