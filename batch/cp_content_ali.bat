d1982f3c69f4b2c59e6396fc0929a6cf746254c4
favedit <favedit@hotmail.com>
2015-04-08 23:22:49 +0800

    代码修正。

--- batch/cp_content_ali.bat
+++ batch/cp_content_ali.bat
@@ -4,9 +4,9 @@
 @SET SOURCE=%HOME%\MoCloud
 @SET TARGET=%HOME%\Aliyun-Svn
 
-xcopy /E /I /Y %SOURCE%\mp-cloud-content\webroot %TARGET%\cloud.content\webroot
+xcopy /E /I /Y %SOURCE%\mp-cloud-content\webroot  %TARGET%\cloud.content\webroot
 
-xcopy /E /I /Y %HOME%\MoScript\source\home        %TARGET%\webroot
-xcopy /E /I /Y %HOME%\MoScript\source\ajs\release %TARGET%\webroot\engine\ajs
+xcopy /E /I /Y %HOME%\MoScript\source\acs         %TARGET%\webroot\engine\acs
 xcopy /E /I /Y %HOME%\MoScript\source\ar3\shader  %TARGET%\webroot\engine\ar3\shader
+xcopy /E /I /Y %HOME%\MoScript\source\ajs\release %TARGET%\webroot\engine\ajs
 xcopy /E /I /Y %HOME%\MoScript\source\ars         %TARGET%\webroot\engine\ars
