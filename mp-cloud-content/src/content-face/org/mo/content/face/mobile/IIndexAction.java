210a549b06731d7c398b79f612d04ff65884d3ef
sunhuairui <sunrui908@163.com>
2015-07-18 18:13:03 +0800

    [MoCloud] 增加保存浏览器及手机信息接口。

---
+++ mp-cloud-content/src/content-face/org/mo/content/face/mobile/IIndexAction.java
@@ -0,0 +1,28 @@
+package org.mo.content.face.mobile;
+
+import org.mo.data.logic.ILogicContext;
+import org.mo.web.core.container.AContainer;
+import org.mo.web.protocol.context.IWebContext;
+
+//============================================================
+// <P>接口。</P>
+//
+// @author sunhr
+// @version 150718
+//============================================================
+public interface IIndexAction
+{
+   //============================================================
+   // <T>默认逻辑处理。</T>
+   //
+   // @param context 页面环境
+   // @param page 页面
+   //============================================================
+   String construct(IWebContext context,
+                    ILogicContext logicContext,
+                    @AContainer(name = "page") FIndexPage page);
+
+   String putMobileInfo(IWebContext context,
+                        ILogicContext logicContext,
+                        @AContainer(name = "page") FIndexPage page);
+}
