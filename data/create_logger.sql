daf27d38c17f26986bc32dfdbe9c12bf81799435
favedit <favedit@hotmail.com>
2015-07-21 15:59:37 +0800

    数据库结构修正。

--- data/create_logger.sql
+++ data/create_logger.sql
@@ -1,32 +1,19 @@
 -- ------------------------------------------------------------
--- Create table [Logger.Person.User.Operation]
+-- Create table [Logger.Info.Browser.Access]
 -- ------------------------------------------------------------
-DROP TABLE IF EXISTS `LG_PSN_USER_OPERATION`;
-CREATE TABLE `LG_PSN_USER_OPERATION` 
+DROP TABLE IF EXISTS `LG_INF_BROWSER_ACCESS`;
+CREATE TABLE `LG_INF_BROWSER_ACCESS` 
 ( 
    `OUID`                          BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
    `OVLD`                          TINYINT NOT NULL DEFAULT TRUE, 
-   `USER_ID`                       BIGINT NOT NULL, 
-   `OPERATION_CD`                  INTEGER, 
-   `CREATE_USER_ID`                INTEGER, 
+   `GUID`                          VARCHAR(40) NOT NULL, 
+   `BROWSER_ID`                    BIGINT, 
+   `PAGE_URI`                      VARCHAR(800), 
+   `CREATE_USER_ID`                BIGINT, 
    `CREATE_DATE`                   DATETIME, 
-   `UPDATE_USER_ID`                INTEGER, 
+   `UPDATE_USER_ID`                BIGINT, 
    `UPDATE_DATE`                   DATETIME 
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8; 
 
--- ------------------------------------------------------------
--- Create table [Logger.Resource.Application.Download]
--- ------------------------------------------------------------
-DROP TABLE IF EXISTS `LG_RES_APPLICATION_DOWNLOAD`;
-CREATE TABLE `LG_RES_APPLICATION_DOWNLOAD` 
-( 
-   `OUID`                          BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
-   `OVLD`                          TINYINT NOT NULL DEFAULT TRUE, 
-   `APPLICATION_ID`                BIGINT, 
-   `USER_ID`                       BIGINT, 
-   `CREATE_USER_ID`                INTEGER, 
-   `CREATE_DATE`                   DATETIME, 
-   `UPDATE_USER_ID`                INTEGER, 
-   `UPDATE_DATE`                   DATETIME 
-) ENGINE=MyISAM DEFAULT CHARSET=utf8; 
-
+ALTER TABLE LG_INF_BROWSER_ACCESS 
+   ADD CONSTRAINT LG_INF_BRW_ACS_UK_GID UNIQUE ( GUID ); 
---
+++ mo-5-cloud/src/cloud-data/com/cyou/gccloud/data/logger/FLoggerInfoBrowserAccessUnit.java
@@ -0,0 +1,483 @@
+package com.cyou.gccloud.data.logger;
+
+import java.util.Map;
+import org.mo.com.collections.FRow;
+import org.mo.com.lang.IStringPair;
+import org.mo.com.lang.RBoolean;
+import org.mo.com.lang.RLong;
+import org.mo.com.lang.RString;
+import org.mo.com.lang.type.TDateTime;
+import org.mo.core.aop.face.ASourceMachine;
+import org.mo.data.logic.FLogicUnit;
+
+//============================================================
+// <T>信息浏览器访问逻辑单元。</T>
+//============================================================
+@ASourceMachine
+public class FLoggerInfoBrowserAccessUnit
+      extends FLogicUnit
+{
+   // 存储字段对象标识的定义。
+   private long __ouid;
+
+   // 字段对象标识的定义。
+   protected long _ouid;
+
+   // 存储字段有效性的定义。
+   private boolean __ovld;
+
+   // 字段有效性的定义。
+   protected boolean _ovld;
+
+   // 存储字段全局唯一标识的定义。
+   private String __guid;
+
+   // 字段全局唯一标识的定义。
+   protected String _guid;
+
+   // 存储字段浏览器编号的定义。
+   private long __browserId;
+
+   // 字段浏览器编号的定义。
+   protected long _browserId;
+
+   // 存储字段访问页面的定义。
+   private String __pageUri;
+
+   // 字段访问页面的定义。
+   protected String _pageUri;
+
+   // 存储字段创建用户标识的定义。
+   private long __createUserId;
+
+   // 字段创建用户标识的定义。
+   protected long _createUserId;
+
+   // 存储字段创建日期的定义。
+   private TDateTime __createDate = new TDateTime();
+
+   // 字段创建日期的定义。
+   protected TDateTime _createDate = new TDateTime();
+
+   // 存储字段更新者标识的定义。
+   private long __updateUserId;
+
+   // 字段更新者标识的定义。
+   protected long _updateUserId;
+
+   // 存储字段更新时间的定义。
+   private TDateTime __updateDate = new TDateTime();
+
+   // 字段更新时间的定义。
+   protected TDateTime _updateDate = new TDateTime();
+
+   //============================================================
+   // <T>构造信息浏览器访问逻辑单元。</T>
+   //============================================================
+   public FLoggerInfoBrowserAccessUnit(){
+   }
+
+   //============================================================
+   // <T>判断对象标识的数据是否改变。</T>
+   //
+   // @return 数据内容
+   //============================================================
+   public boolean isOuidChanged(){
+      return __ouid != _ouid;
+   }
+
+   //============================================================
+   // <T>获得对象标识的数据内容。</T>
+   //
+   // @return 数据内容
+   //============================================================
+   public long ouid(){
+      return _ouid;
+   }
+
+   //============================================================
+   // <T>设置对象标识的数据内容。</T>
+   //
+   // @param value 数据内容
+   //============================================================
+   public void setOuid(long value){
+      _ouid = value;
+   }
+
+   //============================================================
+   // <T>判断有效性的数据是否改变。</T>
+   //
+   // @return 数据内容
+   //============================================================
+   public boolean isOvldChanged(){
+      return __ovld != _ovld;
+   }
+
+   //============================================================
+   // <T>获得有效性的数据内容。</T>
+   //
+   // @return 数据内容
+   //============================================================
+   public boolean ovld(){
+      return _ovld;
+   }
+
+   //============================================================
+   // <T>设置有效性的数据内容。</T>
+   //
+   // @param value 数据内容
+   //============================================================
+   public void setOvld(boolean value){
+      _ovld = value;
+   }
+
+   //============================================================
+   // <T>判断全局唯一标识的数据是否改变。</T>
+   //
+   // @return 数据内容
+   //============================================================
+   public boolean isGuidChanged(){
+      return !RString.equals(__guid, _guid);
+   }
+
+   //============================================================
+   // <T>获得全局唯一标识的数据内容。</T>
+   //
+   // @return 数据内容
+   //============================================================
+   public String guid(){
+      return _guid;
+   }
+
+   //============================================================
+   // <T>设置全局唯一标识的数据内容。</T>
+   //
+   // @param value 数据内容
+   //============================================================
+   public void setGuid(String value){
+      _guid = value;
+   }
+
+   //============================================================
+   // <T>判断浏览器编号的数据是否改变。</T>
+   //
+   // @return 数据内容
+   //============================================================
+   public boolean isBrowserIdChanged(){
+      return __browserId != _browserId;
+   }
+
+   //============================================================
+   // <T>获得浏览器编号的数据内容。</T>
+   //
+   // @return 数据内容
+   //============================================================
+   public long browserId(){
+      return _browserId;
+   }
+
+   //============================================================
+   // <T>设置浏览器编号的数据内容。</T>
+   //
+   // @param value 数据内容
+   //============================================================
+   public void setBrowserId(long value){
+      _browserId = value;
+   }
+
+   //============================================================
+   // <T>判断访问页面的数据是否改变。</T>
+   //
+   // @return 数据内容
+   //============================================================
+   public boolean isPageUriChanged(){
+      return !RString.equals(__pageUri, _pageUri);
+   }
+
+   //============================================================
+   // <T>获得访问页面的数据内容。</T>
+   //
+   // @return 数据内容
+   //============================================================
+   public String pageUri(){
+      return _pageUri;
+   }
+
+   //============================================================
+   // <T>设置访问页面的数据内容。</T>
+   //
+   // @param value 数据内容
+   //============================================================
+   public void setPageUri(String value){
+      _pageUri = value;
+   }
+
+   //============================================================
+   // <T>判断创建用户标识的数据是否改变。</T>
+   //
+   // @return 数据内容
+   //============================================================
+   public boolean isCreateUserIdChanged(){
+      return __createUserId != _createUserId;
+   }
+
+   //============================================================
+   // <T>获得创建用户标识的数据内容。</T>
+   //
+   // @return 数据内容
+   //============================================================
+   public long createUserId(){
+      return _createUserId;
+   }
+
+   //============================================================
+   // <T>设置创建用户标识的数据内容。</T>
+   //
+   // @param value 数据内容
+   //============================================================
+   public void setCreateUserId(long value){
+      _createUserId = value;
+   }
+
+   //============================================================
+   // <T>判断创建日期的数据是否改变。</T>
+   //
+   // @return 数据内容
+   //============================================================
+   public boolean isCreateDateChanged(){
+      return !__createDate.equals(_createDate);
+   }
+
+   //============================================================
+   // <T>获得创建日期的数据内容。</T>
+   //
+   // @return 数据内容
+   //============================================================
+   public TDateTime createDate(){
+      return _createDate;
+   }
+
+   //============================================================
+   // <T>设置创建日期的数据内容。</T>
+   //
+   // @param value 数据内容
+   //============================================================
+   public void setCreateDate(TDateTime value){
+      _createDate = value;
+   }
+
+   //============================================================
+   // <T>判断更新者标识的数据是否改变。</T>
+   //
+   // @return 数据内容
+   //============================================================
+   public boolean isUpdateUserIdChanged(){
+      return __updateUserId != _updateUserId;
+   }
+
+   //============================================================
+   // <T>获得更新者标识的数据内容。</T>
+   //
+   // @return 数据内容
+   //============================================================
+   public long updateUserId(){
+      return _updateUserId;
+   }
+
+   //============================================================
+   // <T>设置更新者标识的数据内容。</T>
+   //
+   // @param value 数据内容
+   //============================================================
+   public void setUpdateUserId(long value){
+      _updateUserId = value;
+   }
+
+   //============================================================
+   // <T>判断更新时间的数据是否改变。</T>
+   //
+   // @return 数据内容
+   //============================================================
+   public boolean isUpdateDateChanged(){
+      return !__updateDate.equals(_updateDate);
+   }
+
+   //============================================================
+   // <T>获得更新时间的数据内容。</T>
+   //
+   // @return 数据内容
+   //============================================================
+   public TDateTime updateDate(){
+      return _updateDate;
+   }
+
+   //============================================================
+   // <T>设置更新时间的数据内容。</T>
+   //
+   // @param value 数据内容
+   //============================================================
+   public void setUpdateDate(TDateTime value){
+      _updateDate = value;
+   }
+
+   //============================================================
+   // <T>根据名称获得内容。</T>
+   //
+   // @param name 名称
+   // @return 内容
+   //============================================================
+   @Override
+   public String get(String name){
+      switch(name){
+         case "ouid":
+            return Long.toString(_ouid);
+         case "ovld":
+            return RBoolean.toString(_ovld);
+         case "guid":
+            return _guid;
+         case "browser_id":
+            return Long.toString(_browserId);
+         case "page_uri":
+            return _pageUri;
+         case "create_user_id":
+            return Long.toString(_createUserId);
+         case "create_date":
+            return _createDate.toString();
+         case "update_user_id":
+            return Long.toString(_updateUserId);
+         case "update_date":
+            return _updateDate.toString();
+      }
+      return null;
+   }
+
+   //============================================================
+   // <T>根据名称设置内容。</T>
+   //
+   // @param name 名称
+   // @param value 内容
+   //============================================================
+   @Override
+   public void set(String name,
+                   String value){
+      switch(name){
+         case "ouid":
+            _ouid = RLong.parse(value);
+            break;
+         case "ovld":
+            _ovld = RBoolean.parse(value);
+            break;
+         case "guid":
+            _guid = value;
+            break;
+         case "browser_id":
+            _browserId = RLong.parse(value);
+            break;
+         case "page_uri":
+            _pageUri = value;
+            break;
+         case "create_user_id":
+            _createUserId = RLong.parse(value);
+            break;
+         case "create_date":
+            _createDate.parse(value);
+            break;
+         case "update_user_id":
+            _updateUserId = RLong.parse(value);
+            break;
+         case "update_date":
+            _updateDate.parse(value);
+            break;
+      }
+   }
+
+   //============================================================
+   // <T>加载行记录。</T>
+   //
+   // @param row 行记录
+   //============================================================
+   @Override
+   public void load(FRow row){
+      super.load(row);
+      for(IStringPair pair : row){
+         String name = pair.name();
+         String value = pair.value();
+         switch(name){
+            case "ouid":
+               __ouid = RLong.parse(value);
+               _ouid = __ouid;
+               break;
+            case "ovld":
+               __ovld = RBoolean.parse(value);
+               _ovld = __ovld;
+               break;
+            case "guid":
+               __guid = value;
+               _guid = __guid;
+               break;
+            case "browser_id":
+               __browserId = RLong.parse(value);
+               _browserId = __browserId;
+               break;
+            case "page_uri":
+               __pageUri = value;
+               _pageUri = __pageUri;
+               break;
+            case "create_user_id":
+               __createUserId = RLong.parse(value);
+               _createUserId = __createUserId;
+               break;
+            case "create_date":
+               __createDate.parse(value);
+               _createDate.assign(__createDate);
+               break;
+            case "update_user_id":
+               __updateUserId = RLong.parse(value);
+               _updateUserId = __updateUserId;
+               break;
+            case "update_date":
+               __updateDate.parse(value);
+               _updateDate.assign(__updateDate);
+               break;
+         }
+      }
+   }
+
+   //============================================================
+   // <T>存储行记录。</T>
+   //
+   // @param row 行记录
+   //============================================================
+   @Override
+   public void save(FRow row){
+      super.load(row);
+      row.set("ouid", _ouid);
+      row.set("ovld", _ovld);
+      row.set("guid", _guid);
+      row.set("browserId", _browserId);
+      row.set("pageUri", _pageUri);
+      row.set("createUserId", _createUserId);
+      row.set("createDate", _createDate);
+      row.set("updateUserId", _updateUserId);
+      row.set("updateDate", _updateDate);
+   }
+
+   //============================================================
+   // <T>保存对照表。</T>
+   //
+   // @param map 对照表
+   //============================================================
+   @Override
+   public void saveMap(Map<String, String> map){
+      super.saveMap(map);
+      map.put("ouid", RLong.toString(_ouid));
+      map.put("ovld", RBoolean.toString(_ovld));
+      map.put("guid", _guid);
+      map.put("browserId", RLong.toString(_browserId));
+      map.put("pageUri", _pageUri);
+      map.put("createUserId", RLong.toString(_createUserId));
+      map.put("createDate", _createDate.format("YYYY-MM-DD HH24:MI:SS"));
+      map.put("updateUserId", RLong.toString(_updateUserId));
+      map.put("updateDate", _updateDate.format("YYYY-MM-DD HH24:MI:SS"));
+   }
+}
