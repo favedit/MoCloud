2788e531773404f5293d1f6152ec2db5b7c7c689
favedit <favedit@hotmail.com>
2015-07-27 09:41:22 +0800

    代码修正。

--- copy_mojs.bat
+++
@@ -1,8 +0,0 @@
-@CLS
-
-@SET HOME_SOURCE=D:\Microbject\MoScript\source
-@SET HOME_TARGET=D:\Microbject\MoCloud
-
-XCOPY /E /Y %HOME_SOURCE%\ajs\release\ %HOME_TARGET%\mp-eai-content\webroot\ajs
-XCOPY /E /Y %HOME_SOURCE%\eai-release  %HOME_TARGET%\mp-eai-content\webroot
-
