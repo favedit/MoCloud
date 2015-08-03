f61c019cc89f7b0c4eca173b2a52297a40f48a0f
favedit <favedit@hotmail.com>
2015-08-03 13:45:37 +0800

    代码修正。

--- mp-eai-batch/src/core/com/ahyc/eai/batch/financial/RStatisticsCalculater.java
+++ mp-eai-batch/src/core/com/ahyc/eai/batch/financial/RStatisticsCalculater.java
@@ -1,7 +1,9 @@
 package com.ahyc.eai.batch.financial;
 
 import org.mo.com.lang.FAttributes;
+import org.mo.com.lang.FFatalError;
 import org.mo.com.lang.FObject;
+import org.mo.com.lang.RString;
 import org.mo.com.logging.ILogger;
 import org.mo.com.logging.RLogger;
 import org.mo.core.aop.RAop;
@@ -21,11 +23,25 @@ public class RStatisticsCalculater
    // <T>投资阶段处理。</T>
    //============================================================
    public static void main(String[] args){
+      // 打印参数
+      System.out.println("-- Statistics calculater start");
+      int count = args.length;
+      for(int i = 0; i < count; i++){
+         System.out.println(" [" + i + "] " + args[i]);
+      }
+      // 检查参数
+      if(args.length != 1){
+         throw new FFatalError("Argements count failure");
+      }
+      String modeCd = RString.trim(args[0]);
+      if(!modeCd.equals("test") && !modeCd.equals("online")){
+         throw new FFatalError("Mode type failure.");
+      }
       // 设置属性
       FAttributes attributes = RAop.configConsole().defineCollection().attributes();
       attributes.set("application", "/data/eai/eai.batch");
       // 加载配置
-      RAop.initialize("/data/eai/eai.batch/webroot/WEB-INF/classes/application-test.xml");
+      RAop.initialize("/data/eai/eai.batch/webroot/WEB-INF/classes/application-" + modeCd + ".xml");
       // 逻辑处理
       IDatabaseConsole databaseConsole = RAop.find(IDatabaseConsole.class);
       // 启动监视器
