8310ed61e2f77fd157626139d2e03c27a17314da
favedit <favedit@hotmail.com>
2015-08-05 10:54:02 +0800

    数据库密码更换。

--- mp-eai-batch/src/utility/org/mo/eai/RStringTest.java
+++ mp-eai-batch/src/utility/org/mo/eai/RStringTest.java
@@ -1,14 +1,10 @@
 package org.mo.eai;
 
-import org.mo.com.encoding.REncryption;
-
 public class RStringTest
 {
    public static void main(String[] args){
-      String source = REncryption.encodeString("ezubao_ai", "c20c1fc7158f77oBb6d");
-      System.out.println(source);
-      //      String value = REncryption.decodeString("test", source);
-      //      System.out.println(value);
+      // System.out.println(RUuid.simpleUuid());
+      // String value = REncryption.decodeString("test", source);
+      // System.out.println(value);
    }
-
 }
