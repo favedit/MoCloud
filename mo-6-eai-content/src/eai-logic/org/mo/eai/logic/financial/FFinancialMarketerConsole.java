f83776b6448158c18f7729b0b1dc9f2cc75ec3ee
favedit <favedit@hotmail.com>
2015-08-25 17:02:47 +0800

    增加获得理财师信息。
    Change-Id: I3e7fa73d0e6b7b1ba9019686d064c1c60339e443

---
+++ mo-6-eai-content/src/eai-logic/org/mo/eai/logic/financial/FFinancialMarketerConsole.java
@@ -0,0 +1,61 @@
+package org.mo.eai.logic.financial;
+
+import org.mo.com.collections.FRow;
+import org.mo.com.console.FConsole;
+import org.mo.com.data.FSql;
+import org.mo.com.data.ISqlConnection;
+import org.mo.data.logic.ILogicContext;
+
+//============================================================
+// <T>金融控制台。</T>
+//============================================================
+public class FFinancialMarketerConsole
+      extends FConsole
+      implements
+         IFinancialMarketerConsole
+{
+   // 资源访问接口
+   //private static IResource _resource = RResource.find(FFinancialMarketerConsole.class);
+
+   //============================================================
+   // <T>根据理登录名称查找理财师信息。</T>
+   //
+   // @param logicContext 逻辑环境
+   // @param passport 登录名称
+   // @return 理财师信息
+   //============================================================
+   @Override
+   public FFinancialMarketerInfo findInfo(ILogicContext logicContext,
+                                          String passport){
+      ISqlConnection connection = logicContext.activeConnection("");
+      // 查找用户编号
+      FSql findSql = new FSql("SELECT id FROM lzh_members WHERE user_name={passport}");
+      findSql.bindString("passport", passport);
+      FRow memberRow = connection.find(findSql);
+      if(memberRow == null){
+         return null;
+      }
+      // 查找理财师
+      long memberId = memberRow.getLong("id");
+      FSql managerSql = new FSql("SELECT id FROM idcard WHERE uid={user_id} status != 1");
+      managerSql.bindLong("user_id", memberId);
+      FRow managerRow = connection.find(findSql);
+      if(managerRow == null){
+         return null;
+      }
+      // 查找用户信息
+      FSql memberInfoSql = new FSql("SELECT real_name,cell_phone,idcard FROM lzh_member_info WHERE uid={user_id}");
+      memberInfoSql.bindLong("user_id", memberId);
+      FRow memberInfoRow = connection.find(findSql);
+      if(memberInfoRow == null){
+         return null;
+      }
+      // 返回内容
+      FFinancialMarketerInfo info = new FFinancialMarketerInfo();
+      info.setPassport(passport);
+      info.setLabel(memberInfoRow.get("real_name"));
+      info.setPhone(memberInfoRow.get("cell_phone"));
+      info.setCard(memberInfoRow.get("idcard"));
+      return info;
+   }
+}
---
+++ mo-6-eai-content/src/eai-logic/org/mo/eai/logic/financial/IFinancialMarketerConsole.java
@@ -0,0 +1,19 @@
+package org.mo.eai.logic.financial;
+
+import org.mo.data.logic.ILogicContext;
+
+//============================================================
+// <T>金融理财师控制台接口。</T>
+//============================================================
+public interface IFinancialMarketerConsole
+{
+   //============================================================
+   // <T>根据理登录名称查找理财师信息。</T>
+   //
+   // @param logicContext 逻辑环境
+   // @param passport 登录名称
+   // @return 理财师信息
+   //============================================================
+   FFinancialMarketerInfo findInfo(ILogicContext logicContext,
+                                   String passport);
+}
