8de023294d198e8696ccd1e0acf244048ff67a4e
sunhuairui <sunrui908@163.com>
2015-09-11 16:52:25 +0800

    部门信息excel表格导入数据库
    Change-Id: Ie0348e11e264801238d114e6fcc11cc337e120c4

--- mp-eai-manage/src/content-face/org/mo/content/face/FIndexAction.java
+++ mp-eai-manage/src/content-face/org/mo/content/face/FIndexAction.java
@@ -7,8 +7,12 @@ import org.apache.poi.hssf.usermodel.HSSFCell;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.mo.com.lang.FAttributes;
 import org.mo.content.core.product.financial.IDepartmentConsole;
+import org.mo.core.aop.RAop;
 import org.mo.core.aop.face.ALink;
+import org.mo.data.logic.FLogicContext;
+import org.mo.eng.data.IDatabaseConsole;
 import org.mo.eng.data.common.ISqlContext;
 
 //============================================================
@@ -41,28 +45,54 @@ public class FIndexAction
    public static void main(String[] args){
       try{
          InputStream fis = new FileInputStream("D:\\Microbject\\MoScript\\data\\ezubo_department.xls");
+         // 设置属性
+         FAttributes attributes = RAop.configConsole().defineCollection().attributes();
+         attributes.set("application", "D:/Microbject/MoCloud/");
+         RAop.initialize("D:/Microbject/MoCloud/mp-eai-manage/src/config/application-work.xml");
+
+         IDatabaseConsole databaseConsole = RAop.find(IDatabaseConsole.class);
+         FLogicContext logicContext = new FLogicContext(databaseConsole);
+         IDepartmentConsole console = RAop.find(IDepartmentConsole.class);
+
          HSSFWorkbook wb = new HSSFWorkbook(fis);
          HSSFSheet s = wb.getSheet("Sheet1");
          int rowLength = s.getLastRowNum() + 1;
          String department = "";
          String temp = "";
-         for(int i = 1; i < rowLength; i++){
-            HSSFRow row = s.getRow(i);
+         for(int i = 0; i < rowLength; i++){
+            HSSFRow row = s.getRow(i + 2);
             if(row != null){
-               HSSFCell cell1 = row.getCell(0);
-               temp = String.valueOf(cell1.getStringCellValue());
-               if(temp.length() > 0){
+               //获取每一列
+               HSSFCell cell0 = row.getCell(0);
+               temp = String.valueOf(cell0.getStringCellValue());
+               if(temp.length() > 1){
                   department = temp;
                }
-               System.out.println(department + "--");
+               HSSFCell regionCell = row.getCell(1);
+               HSSFCell companCell = row.getCell(2);
+               HSSFCell provinceCell = row.getCell(3);
+               HSSFCell cityCell = row.getCell(4);
+               HSSFCell detailCell = row.getCell(5);
+               HSSFCell longitudeCell = row.getCell(6);
+               HSSFCell latitudeCell = row.getCell(7);
+               HSSFCell departmentPhoneCell = row.getCell(8);
+               HSSFCell leaderLabelCell = row.getCell(9);
+               //生成对象增加数据库
+               FDataFinancialDepartmentUnit unit = new FDataFinancialDepartmentUnit();
+               unit.setDepartmentLabel(department);
+               unit.setRegionLabel(regionCell.getStringCellValue());
+               unit.setLabel(companCell.getStringCellValue());
+               unit.setProvinceLabel(provinceCell.getStringCellValue());
+               unit.setCityLabel(cityCell.getStringCellValue());
+               unit.setDetailAddress(detailCell.getStringCellValue());
+               unit.setLocationLongitude(longitudeCell != null ? longitudeCell.getNumericCellValue() : 0.00);
+               unit.setLocationLatitude(latitudeCell != null ? latitudeCell.getNumericCellValue() : 0.00);
+               unit.setDepartmentPhone(String.valueOf(departmentPhoneCell.getStringCellValue()));
+               unit.setLeaderLabel(leaderLabelCell.getStringCellValue());
+               console.doInsert(logicContext, unit);
             }
-            FDataFinancialDepartmentUnit unit = new FDataFinancialDepartmentUnit();
-            unit.setDepartmentLabel(department);
          }
-         System.out.println(s.getLastRowNum());
-         //         HSSFRow row = s.getFirstRowNum();
       }catch(Exception e){
-         // TODO Auto-generated catch block
          e.printStackTrace();
       }
    }
