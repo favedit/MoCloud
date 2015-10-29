93ad5e1d04afec5eaed648cebe2422100303e11d
favedit <favedit@hotmail.com>
2015-10-29 13:18:16 +0800

    删除双10创演示文件。

---
+++ mp-eai-content/src/content-face-test/org/mo/content/face/mobile/logic/news/TestFNewsAction.java
@@ -0,0 +1,23 @@
+package org.mo.content.face.mobile.logic.news;
+
+import java.io.IOException;
+
+public class TestFNewsAction
+{
+
+   @Test
+   public void testGetNewInfo() throws ClientProtocolException, IOException{
+      CloseableHttpClient httpclient = HttpClients.createDefault();
+      String url = "http://10.13.0.100:8010/Index.wa?guid=FE3814B38EA5477BAF6900FB927324B2";
+      HttpGet post = new HttpGet(url);
+      CloseableHttpResponse response = httpclient.execute(post);
+      HttpEntity entity = response.getEntity();
+      if(entity != null){
+         String responseContent = EntityUtils.toString(entity);
+         String result = new String(responseContent.getBytes("utf-8"));
+         System.out.println(result);
+      }
+
+   }
+
+}
