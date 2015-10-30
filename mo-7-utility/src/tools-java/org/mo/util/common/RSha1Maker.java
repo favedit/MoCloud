fd79694ece2284b926e304e05bedd7260aa1aba7
favedit <favedit@hotmail.com>
2015-10-30 17:47:00 +0800

    代码修正。

---
+++ mo-7-utility/src/tools-java/org/mo/util/common/RSha1Maker.java
@@ -0,0 +1,10 @@
+package org.mo.util.common;
+
+import org.mo.com.encoding.RSha1;
+
+public class RSha1Maker
+{
+   public static void main(String[] args){
+      System.out.println(RSha1.encode("sha1"));
+   }
+}
