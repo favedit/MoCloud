cbcac6252ce03755db6417fba5c20249aae3043a
sunhuairui <sunrui908@163.com>
2015-11-04 16:38:37 +0800

    [服务器] 创建产品期次对象
    Change-Id: I1c707f9a97e717da8c36d7933b71830b79c001d7

---
+++ mo-5-cloud/src/cloud-data/com/cyou/gccloud/data/calculate/FCalculateFinancialProductPeriodUnit.java
@@ -0,0 +1,558 @@
+package com.cyou.gccloud.data.calculate;
+
+import java.util.Map;
+import org.mo.com.collections.FRow;
+import org.mo.com.io.IDataInput;
+import org.mo.com.io.IDataOutput;
+import org.mo.com.lang.IStringPair;
+import org.mo.com.lang.RBoolean;
+import org.mo.com.lang.RInteger;
+import org.mo.com.lang.RLong;
+import org.mo.com.lang.RString;
+import org.mo.com.lang.reflect.RClass;
+import org.mo.com.lang.type.TDateTime;
+import org.mo.core.aop.face.ASourceMachine;
+import org.mo.data.logic.FLogicUnit;
+
+//============================================================
+// <T>产品期数关系表逻辑单元。</T>
+//============================================================
+@ASourceMachine
+public class FCalculateFinancialProductPeriodUnit
+      extends FLogicUnit
+{
+   // 存储字段对象标识的定义。
+   private long __ouid;
+
+   // 字段对象标识的定义。
+   protected long _ouid;
+
+   // 存储字段有效性的定义。
+   private boolean __ovld;
+
+   // 字段有效性的定义。
+   protected boolean _ovld;
+
+   // 存储字段对象唯一标识的定义。
+   private String __guid;
+
+   // 字段对象唯一标识的定义。
+   protected String _guid;
+
+   // 存储字段产品名称的定义。
+   private String __productCode;
+
+   // 字段产品名称的定义。
+   protected String _productCode;
+
+   // 存储字段期次编号的定义。
+   private int __productIssue;
+
+   // 字段期次编号的定义。
+   protected int _productIssue;
+
+   // 存储字段创建用户标识的定义。
+   private long __createUserId;
+
+   // 字段创建用户标识的定义。
+   protected long _createUserId;
+
+   // 存储字段创建日期的定义。
+   private TDateTime __createDate = new TDateTime();
+
+   // 字段创建日期的定义。
+   protected TDateTime _createDate = new TDateTime();
+
+   // 存储字段更新者标识的定义。
+   private long __updateUserId;
+
+   // 字段更新者标识的定义。
+   protected long _updateUserId;
+
+   // 存储字段更新时间的定义。
+   private TDateTime __updateDate = new TDateTime();
+
+   // 字段更新时间的定义。
+   protected TDateTime _updateDate = new TDateTime();
+
+   //============================================================
+   // <T>构造产品期数关系表逻辑单元。</T>
+   //============================================================
+   public FCalculateFinancialProductPeriodUnit(){
+   }
+
+   //============================================================
+   // <T>判断对象标识的数据是否改变。</T>
+   //
+   // @return 数据内容
+   //============================================================
+   public boolean isOuidChanged(){
+      return __ouid != _ouid;
+   }
+
+   //============================================================
+   // <T>获得对象标识的数据内容。</T>
+   //
+   // @return 数据内容
+   //============================================================
+   public long ouid(){
+      return _ouid;
+   }
+
+   //============================================================
+   // <T>设置对象标识的数据内容。</T>
+   //
+   // @param value 数据内容
+   //============================================================
+   public void setOuid(long value){
+      _ouid = value;
+   }
+
+   //============================================================
+   // <T>判断有效性的数据是否改变。</T>
+   //
+   // @return 数据内容
+   //============================================================
+   public boolean isOvldChanged(){
+      return __ovld != _ovld;
+   }
+
+   //============================================================
+   // <T>获得有效性的数据内容。</T>
+   //
+   // @return 数据内容
+   //============================================================
+   public boolean ovld(){
+      return _ovld;
+   }
+
+   //============================================================
+   // <T>设置有效性的数据内容。</T>
+   //
+   // @param value 数据内容
+   //============================================================
+   public void setOvld(boolean value){
+      _ovld = value;
+   }
+
+   //============================================================
+   // <T>判断对象唯一标识的数据是否改变。</T>
+   //
+   // @return 数据内容
+   //============================================================
+   public boolean isGuidChanged(){
+      return !RString.equals(__guid, _guid);
+   }
+
+   //============================================================
+   // <T>获得对象唯一标识的数据内容。</T>
+   //
+   // @return 数据内容
+   //============================================================
+   public String guid(){
+      return _guid;
+   }
+
+   //============================================================
+   // <T>设置对象唯一标识的数据内容。</T>
+   //
+   // @param value 数据内容
+   //============================================================
+   public void setGuid(String value){
+      _guid = value;
+   }
+
+   //============================================================
+   // <T>判断产品名称的数据是否改变。</T>
+   //
+   // @return 数据内容
+   //============================================================
+   public boolean isProductCodeChanged(){
+      return !RString.equals(__productCode, _productCode);
+   }
+
+   //============================================================
+   // <T>获得产品名称的数据内容。</T>
+   //
+   // @return 数据内容
+   //============================================================
+   public String productCode(){
+      return _productCode;
+   }
+
+   //============================================================
+   // <T>设置产品名称的数据内容。</T>
+   //
+   // @param value 数据内容
+   //============================================================
+   public void setProductCode(String value){
+      _productCode = value;
+   }
+
+   //============================================================
+   // <T>判断期次编号的数据是否改变。</T>
+   //
+   // @return 数据内容
+   //============================================================
+   public boolean isProductIssueChanged(){
+      return __productIssue != _productIssue;
+   }
+
+   //============================================================
+   // <T>获得期次编号的数据内容。</T>
+   //
+   // @return 数据内容
+   //============================================================
+   public int productIssue(){
+      return _productIssue;
+   }
+
+   //============================================================
+   // <T>设置期次编号的数据内容。</T>
+   //
+   // @param value 数据内容
+   //============================================================
+   public void setProductIssue(int value){
+      _productIssue = value;
+   }
+
+   //============================================================
+   // <T>判断创建用户标识的数据是否改变。</T>
+   //
+   // @return 数据内容
+   //============================================================
+   public boolean isCreateUserIdChanged(){
+      return __createUserId != _createUserId;
+   }
+
+   //============================================================
+   // <T>获得创建用户标识的数据内容。</T>
+   //
+   // @return 数据内容
+   //============================================================
+   public long createUserId(){
+      return _createUserId;
+   }
+
+   //============================================================
+   // <T>设置创建用户标识的数据内容。</T>
+   //
+   // @param value 数据内容
+   //============================================================
+   public void setCreateUserId(long value){
+      _createUserId = value;
+   }
+
+   //============================================================
+   // <T>判断创建日期的数据是否改变。</T>
+   //
+   // @return 数据内容
+   //============================================================
+   public boolean isCreateDateChanged(){
+      return !__createDate.equals(_createDate);
+   }
+
+   //============================================================
+   // <T>获得创建日期的数据内容。</T>
+   //
+   // @return 数据内容
+   //============================================================
+   public TDateTime createDate(){
+      return _createDate;
+   }
+
+   //============================================================
+   // <T>设置创建日期的数据内容。</T>
+   //
+   // @param value 数据内容
+   //============================================================
+   public void setCreateDate(TDateTime value){
+      _createDate = value;
+   }
+
+   //============================================================
+   // <T>判断更新者标识的数据是否改变。</T>
+   //
+   // @return 数据内容
+   //============================================================
+   public boolean isUpdateUserIdChanged(){
+      return __updateUserId != _updateUserId;
+   }
+
+   //============================================================
+   // <T>获得更新者标识的数据内容。</T>
+   //
+   // @return 数据内容
+   //============================================================
+   public long updateUserId(){
+      return _updateUserId;
+   }
+
+   //============================================================
+   // <T>设置更新者标识的数据内容。</T>
+   //
+   // @param value 数据内容
+   //============================================================
+   public void setUpdateUserId(long value){
+      _updateUserId = value;
+   }
+
+   //============================================================
+   // <T>判断更新时间的数据是否改变。</T>
+   //
+   // @return 数据内容
+   //============================================================
+   public boolean isUpdateDateChanged(){
+      return !__updateDate.equals(_updateDate);
+   }
+
+   //============================================================
+   // <T>获得更新时间的数据内容。</T>
+   //
+   // @return 数据内容
+   //============================================================
+   public TDateTime updateDate(){
+      return _updateDate;
+   }
+
+   //============================================================
+   // <T>设置更新时间的数据内容。</T>
+   //
+   // @param value 数据内容
+   //============================================================
+   public void setUpdateDate(TDateTime value){
+      _updateDate = value;
+   }
+
+   //============================================================
+   // <T>根据名称获得内容。</T>
+   //
+   // @param name 名称
+   // @return 内容
+   //============================================================
+   @Override
+   public String get(String name){
+      switch(name){
+         case "ouid":
+            return Long.toString(_ouid);
+         case "ovld":
+            return RBoolean.toString(_ovld);
+         case "guid":
+            return _guid;
+         case "product_code":
+            return _productCode;
+         case "product_issue":
+            return RInteger.toString(_productIssue);
+         case "create_user_id":
+            return Long.toString(_createUserId);
+         case "create_date":
+            return _createDate.toString();
+         case "update_user_id":
+            return Long.toString(_updateUserId);
+         case "update_date":
+            return _updateDate.toString();
+      }
+      return null;
+   }
+
+   //============================================================
+   // <T>根据名称设置内容。</T>
+   //
+   // @param name 名称
+   // @param value 内容
+   //============================================================
+   @Override
+   public void set(String name,
+                   String value){
+      switch(name){
+         case "ouid":
+            _ouid = RLong.parse(value);
+            break;
+         case "ovld":
+            _ovld = RBoolean.parse(value);
+            break;
+         case "guid":
+            _guid = value;
+            break;
+         case "product_code":
+            _productCode = value;
+            break;
+         case "product_issue":
+            _productIssue = RInteger.parse(value);
+            break;
+         case "create_user_id":
+            _createUserId = RLong.parse(value);
+            break;
+         case "create_date":
+            _createDate.parse(value);
+            break;
+         case "update_user_id":
+            _updateUserId = RLong.parse(value);
+            break;
+         case "update_date":
+            _updateDate.parse(value);
+            break;
+      }
+   }
+
+   //============================================================
+   // <T>加载行记录。</T>
+   //
+   // @param row 行记录
+   //============================================================
+   @Override
+   public void load(FRow row){
+      super.load(row);
+      for(IStringPair pair : row){
+         String name = pair.name();
+         String value = pair.value();
+         switch(name){
+            case "ouid":
+               __ouid = RLong.parse(value);
+               _ouid = __ouid;
+               break;
+            case "ovld":
+               __ovld = RBoolean.parse(value);
+               _ovld = __ovld;
+               break;
+            case "guid":
+               __guid = value;
+               _guid = __guid;
+               break;
+            case "product_code":
+               __productCode = value;
+               _productCode = __productCode;
+               break;
+            case "product_issue":
+               __productIssue = RInteger.parse(value);
+               _productIssue = __productIssue;
+               break;
+            case "create_user_id":
+               __createUserId = RLong.parse(value);
+               _createUserId = __createUserId;
+               break;
+            case "create_date":
+               __createDate.parse(value);
+               _createDate.assign(__createDate);
+               break;
+            case "update_user_id":
+               __updateUserId = RLong.parse(value);
+               _updateUserId = __updateUserId;
+               break;
+            case "update_date":
+               __updateDate.parse(value);
+               _updateDate.assign(__updateDate);
+               break;
+         }
+      }
+   }
+
+   //============================================================
+   // <T>存储行记录。</T>
+   //
+   // @param row 行记录
+   //============================================================
+   @Override
+   public void save(FRow row){
+      super.load(row);
+      row.set("ouid", _ouid);
+      row.set("ovld", _ovld);
+      row.set("guid", _guid);
+      row.set("productCode", _productCode);
+      row.set("productIssue", _productIssue);
+      row.set("createUserId", _createUserId);
+      row.set("createDate", _createDate);
+      row.set("updateUserId", _updateUserId);
+      row.set("updateDate", _updateDate);
+   }
+
+   //============================================================
+   // <T>保存对照表。</T>
+   //
+   // @param map 对照表
+   //============================================================
+   @Override
+   public void saveMap(Map<String, String> map){
+      super.saveMap(map);
+      map.put("ouid", RLong.toString(_ouid));
+      map.put("ovld", RBoolean.toString(_ovld));
+      map.put("guid", _guid);
+      map.put("productCode", _productCode);
+      map.put("productIssue", RInteger.toString(_productIssue));
+      map.put("createUserId", RLong.toString(_createUserId));
+      map.put("createDate", _createDate.format("YYYY-MM-DD HH24:MI:SS"));
+      map.put("updateUserId", RLong.toString(_updateUserId));
+      map.put("updateDate", _updateDate.format("YYYY-MM-DD HH24:MI:SS"));
+   }
+
+   //============================================================
+   // <T>反序列化数据到内容。</T>
+   //
+   // @param input 输入流
+   //============================================================
+   @Override
+   public void unserialize(IDataInput input){
+      super.unserialize(input);
+      _ouid = input.readInt64();
+      _ovld = input.readBoolean();
+      _guid = input.readString();
+      _productCode = input.readString();
+      _productIssue = input.readInt32();
+      _createUserId = input.readInt64();
+      _createDate.set(input.readInt64());
+      _updateUserId = input.readInt64();
+      _updateDate.set(input.readInt64());
+   }
+
+   //============================================================
+   // <T>序列化内容到数据。</T>
+   //
+   // @param output 输出流
+   //============================================================
+   @Override
+   public void serialize(IDataOutput output){
+      super.serialize(output);
+      output.writeInt64(_ouid);
+      output.writeBoolean(_ovld);
+      output.writeString(_guid);
+      output.writeString(_productCode);
+      output.writeInt32(_productIssue);
+      output.writeInt64(_createUserId);
+      output.writeInt64(_createDate.get());
+      output.writeInt64(_updateUserId);
+      output.writeInt64(_updateDate.get());
+   }
+
+   //============================================================
+   // <T>复制当前对象。</T>
+   //
+   // @param unit 对象
+   // @return 对象
+   //============================================================
+   @Override
+   public void copy(FLogicUnit logicUnit){
+      super.copy(logicUnit);
+      FCalculateFinancialProductPeriodUnit unit = (FCalculateFinancialProductPeriodUnit)logicUnit;
+      unit.setOuid(_ouid);
+      unit.setOvld(_ovld);
+      unit.setGuid(_guid);
+      unit.setProductCode(_productCode);
+      unit.setProductIssue(_productIssue);
+      unit.setCreateUserId(_createUserId);
+      unit.createDate().assign(_createDate);
+      unit.setUpdateUserId(_updateUserId);
+      unit.updateDate().assign(_updateDate);
+   }
+
+   //============================================================
+   // <T>克隆当前对象。</T>
+   //
+   // @return 对象
+   //============================================================
+   @Override
+   public FCalculateFinancialProductPeriodUnit clone(){
+      FCalculateFinancialProductPeriodUnit unit = RClass.newInstance(FCalculateFinancialProductPeriodUnit.class);
+      copy(unit);
+      return unit;
+   }
+}
