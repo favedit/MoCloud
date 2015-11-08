eb73abd4f6b78ca4133f782ec0539710fc0aad5a
tianlu <15311444853@163.com>
2015-11-08 14:54:22 +0800

    [驾驶舱接口]智慧柯南
    Change-Id: I4a3ebc80d6d3e18922c54ab81735d6bc65ee10ea

--- mp-eai-service/src/content-servlet/com/ahyc/eai/service/cockpit/wisdom/FCockpitWisdomServlet.java
+++ mp-eai-service/src/content-servlet/com/ahyc/eai/service/cockpit/wisdom/FCockpitWisdomServlet.java
@@ -57,10 +57,57 @@ public class FCockpitWisdomServlet
       // 设置输出流
       FByteStream stream = createStream(context);
       FByteStream dataStream = createStream(context);
+      Integer[] child0 = new Integer[12];//1-12月每月的投资赎回人数
+      Double[] child1 = new Double[12];//1-12月所有员工的每月月末投资
+      Integer[] child2 = new Integer[12];//1-12月每月固定时间投资赎回的人数
+      Integer[] child3 = new Integer[12];//1-12月每月理财师投资业绩与绩效业绩的百分比
+      Integer[] child4 = new Integer[12];//1-12月,分公司每月的人员流动性
+      for(int i = 0; i < child0.length; i++){
+         child0[i] = (int)(Math.random() * 100000);
+      }
+      for(int i = 0; i < child1.length; i++){
+         child1[i] = (Math.random() * 100000);
+      }
+      for(int i = 0; i < child2.length; i++){
+         child2[i] = (int)(Math.random() * 100000);
+      }
+      for(int i = 0; i < child3.length; i++){
+         child3[i] = (int)(Math.random() * 100);
+      }
+      for(int i = 0; i < child4.length; i++){
+         child4[i] = (int)(Math.random() * 100);
+      }
+      int count = 5;
+      dataStream.writeInt32(count);//5条智慧标题
+      dataStream.writeString("同一账号反复投资/赎回");//第一条曲线的智慧标题
+      int c0 = child0.length;
+      for(int i = 0; i < c0; i++){
+         dataStream.writeInt32(child0[i]);//1-12月每月投资/赎回的人数
+      }
+      dataStream.writeString("所有员工的月末投资");//第二条曲线的智慧标题
+      int c1 = child1.length;
+      for(int i = 0; i < c1; i++){
+         dataStream.writeDouble(child1[i]);//1-12月每月投资/赎回的人数
+      }
+      dataStream.writeString("每月固定时间内投资/赎回");//第三条曲线的智慧标题
+      int c2 = child2.length;
+      dataStream.writeString("15 24:00");//每月15号的24点整
+      for(int i = 0; i < c2; i++){
+         dataStream.writeDouble(child2[i]);//1-12月每月固定时间投资赎回的人数
+      }
+
+      dataStream.writeString("理财师投资业绩/绩效业绩");//第四条曲线的智慧标题
+      int c3 = child3.length;
+      for(int i = 0; i < c3; i++){
+         dataStream.writeInt32(child3[i]);//1-12月每月理财师投资业绩与绩效业绩的百分比
+      }
+      dataStream.writeString("分公司的人员流动性");//第五条曲线的智慧标题
+      int c4 = child4.length;
+      for(int i = 0; i < c4; i++){
+         dataStream.writeInt32(child4[i]);//1-12月,分公司每月的人员流动性
+      }
       //写入数据
       stream.write(dataStream.memory(), 0, dataStream.position());
-      int count = 5;
-      stream.writeInt32(5);
       //............................................................
       // 保存数据到缓冲中
       updateCacheStream(cacheCode, stream);
